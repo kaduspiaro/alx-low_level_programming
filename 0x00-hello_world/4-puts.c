75f386932c04428dec81119267892079645b5c92
Kadubabari Piaro <kaduspiaro@yahoo.com>
2022-06-06 22:29:11 -0700

    Fifth commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,15 @@
 #include<stdio.h>
 
 /**
-
  * main - return the integal of the function
  *
- *Description:use to print 'Programming is like building a multilingual puzzle'
+ * Description:use to print 'Programming is like building a multilingual puzzle'
  *
  * Return: value of 0
 */
 
 int main(void)
-  
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  
-  return (0);
-  
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 }

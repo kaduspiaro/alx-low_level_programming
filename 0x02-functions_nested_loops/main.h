54f384e060253a7d863f129c9dd44aad4c2bfcb8
Kadubabari Piaro <kaduspiaro@yahoo.com>
2022-06-07 21:31:09 -0700

    main

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN.H
+
+/*
+ * File: main.h
+ * Auth: Kadubabari Piaro
+ * Description: Header file containing declarations of all
+ * the functions used in the 0x02-functions_nested_loops directory
+ *
+ */
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+
+endif /* MAIN_H */
+

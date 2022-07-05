6690f361b21213dd3356ec6819f5e80a2acde8fb
Kadubabari Piaro <kaduspiaro@yahoo.com>
2022-07-05 22:26:10 +0100

    Signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,16 +2,13 @@
 
 /**
  * print_sign - Prints the sign of a number.
- *
  * @n: The number of which the sign will be printed.
  *
  * Return: 1 if the number is greater than zero,
  * 0 if the number is zero
  * -1 if the number is less than zero.
  */
-
 int print_sign(int n)
-  
 {
 	if (n > 0)
 	{
@@ -21,12 +18,11 @@ int print_sign(int n)
 	else if (n == 0)
 	{
 		_putchar('0');
-      		return (0);
+		return (0);
 	}
 	else
 	{
-		_putchar(-1);
+		_putchar('-');
 		return (-1);
 	}
-  
 }

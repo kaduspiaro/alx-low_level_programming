9959ab189fd9c91e3de1b82278c117cb7477aa71
Kadubabari Piaro <kaduspiaro@yahoo.com>
2022-07-29 03:05:50 +0100

    cradded create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	s = (char *)mallooc(size * sizeof(char));
+	s = (char *)malloc(size * sizeof(char));
 	if (s == NULL)
 		return (NULL);
 
